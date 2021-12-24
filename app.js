7373a6bcb1fc39e9fe03fa09294139c7d56c68c2
Alexandre <xandehos@gmail.com>
2021-12-24 10:56:51 -0300

    Quase pronto, aparentemente a API do TVMaze esta off, então não tem como verificar

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert("teste");
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const urlAPI = "http://api.tvmaze.com/search/shows";
+  const searchTerm = form.nextElementSibling.querySelector.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(urlAPI, config);
+  makeImages(res.data);
+  form.nextElementSibling.querySelector.value = "";
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.shows.image) {
+      const img = document.createElement("img");
+      img.src = result.shows.image.medium;
+      document.body.append(img);
+    }
+  }
+};
